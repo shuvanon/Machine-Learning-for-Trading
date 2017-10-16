df6089a7f338d98afc6980d93a6cbf4078314068
shuvanon <razik666@gmail.com>
2017-10-16 18:09:59 +0600

    Compute Max Closing Price

---
+++ ClassWork/Reading and plotting stock data/Compute Max Closing Price.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def get_max_close(symbol):
+
+    df =pd.read_csv("data/{}.csv".format(symbol))
+    return df['Close'].max()
+
+def test_run():
+
+    for symbol in ['AAPL', 'IBM']:
+        print("MAX Close")
+        print(symbol, get_max_close(symbol))
+
+
+if __name__ == "__main__":
+    test_run()
