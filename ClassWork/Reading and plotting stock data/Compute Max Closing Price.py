55af575487603147161bb5490df5defea9b909d5
shuvanon <razik666@gmail.com>
2017-10-16 18:44:55 +0600

    Quiz mean Volume

--- ClassWork/Reading and plotting stock data/Compute Max Closing Price.py
+++ ClassWork/Reading and plotting stock data/Compute Max Closing Price.py
@@ -5,18 +5,18 @@ def get_max_close(symbol):
     df =pd.read_csv("data/{}.csv".format(symbol))
     return df['Close'].max()
 
-def get_min_volume(symbol):
+def get_mean_volume(symbol):
 
     df =pd.read_csv("data/{}.csv".format(symbol))
-    return df['Volume'].min()
+    return df['Volume'].mean()
 
 def test_run():
 
     for symbol in ['AAPL', 'IBM']:
         print("MAX Close")
         print(symbol, get_max_close(symbol))
-        print("Min Volume")
-        print(symbol, get_min_volume(symbol))
+        print("Mean Volume")
+        print(symbol, get_mean_volume(symbol))
 
 
 if __name__ == "__main__":
