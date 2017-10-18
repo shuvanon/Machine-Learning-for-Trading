b7438988e6c3da801e71f7aeb2958d9368f89572
shuvanon <razik666@gmail.com>
2017-10-18 12:58:51 +0600

    Quize and two column ploting

--- ClassWork/Reading and plotting stock data/Plotting Stock Price Data.py
+++ ClassWork/Reading and plotting stock data/Plotting Stock Price Data.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-    df = pd.read_csv("data/AAPL.csv")
-    print(df['Adj Close'])
-    df['Adj Close'].plot()
+    df = pd.read_csv("data/IBM.csv")
+    #print(df['Adj Close'])
+    #df['Adj Close'].plot()
+    #print(df['High'])
+    #df['High'].plot()
+    df[['Close', 'Adj Close']].plot()
     plt.show()
 
 if __name__ == "__main__":
